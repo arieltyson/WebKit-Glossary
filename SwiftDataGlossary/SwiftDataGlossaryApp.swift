d29c24cee948061db728892de5232338f198f907
Ariel Tyson <ariel30190@hotmail.com>
2024-11-06 06:19:37 -0800

    Initial Commit

---
+++ SwiftDataGlossary/SwiftDataGlossaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataGlossaryApp.swift
+//  SwiftDataGlossary
+//
+//  Created by Ariel Tyson on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataGlossaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
